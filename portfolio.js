f02cf3a50578df056c7e58f942123754138f043c
Ben Haynie <benohaynie@gmail.com>
2024-05-05 14:24:43 -0700

    got slider to work and reference my info page by click!

---
+++ portfolio.js
@@ -0,0 +1,22 @@
+function infoPages() {
+    window.location.href="https://ben-olsen-haynie.github.io/Info-Pages/InfoPages.html"
+}
+function artistWebpage() {
+    // window.location.href=""
+    alert('Coming Soon!');
+}
+function familyCalender() {
+    // window.location.href=""
+    alert('Coming Soon!');
+}
+
+
+
+
+
+
+// let project = document.getElementById("infoPages");
+// project.addEventListener("click", function() {
+//     console.log("clicked!")
+//     window.location.href="https://roadmap.sh/frontend";
+// })
