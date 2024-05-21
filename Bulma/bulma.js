b261ade440ff85b44ab8d2ce8992b4435095ef00
Ben Haynie <benohaynie@gmail.com>
2024-05-21 04:05:27 -0700

    Added ProjectsPreview imgs and links

---
+++ Bulma/bulma.js
@@ -0,0 +1,16 @@
+function infoPagesLink() {
+    window.location.href = "https://ben-olsen-haynie.github.io/Info-Pages/InfoPages.html";
+}
+
+function artistProfileLink() {
+    window.location.href = "https://ben-olsen-haynie.github.io/Artist-Profile/ArtistProfile.html";
+}
+
+function f21WebpageLink() {
+    window.location.href = "https://ben-olsen-haynie.github.io/Formula21-Website/Bulma/bulma.html";
+}
+
+function familyEventLink() {
+    // window.location.href = "";
+    alert("Coming Soon!!");
+}
